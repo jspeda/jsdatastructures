b2d7383442ec697d1bcaf03f02891f5e9a5ef445
Joe Spedale <jspedale@gmail.com>
2017-06-04 21:34:44 -0400

    Added Header component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sidebar />
+        <Header />
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const Header = () => (
+      <div className="header">
+        <div className="title">javascript computer science</div>
+        <div className="data-structures">data structures</div>
+        <div className="algorithms">algorithms</div>
+        <div className="about">ABOUT</div>
+      </div>
+)
+
+export default Header;
