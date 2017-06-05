79a4acd1e2985951d74d0935514c3fb5e633859a
Joe Spedale <jspedale@gmail.com>
2017-06-04 21:47:01 -0400

    Installed and setup react-router-dom.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import About from './About';
+import DataStructures from './DataStructures';
+import Algorithms from './Algorithms';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/data-structures" component={DataStructures} />
+        <Route exact path="/algorithms" component={Algorithms} />
+      </div>
+    </Router>
+  )
+}
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
